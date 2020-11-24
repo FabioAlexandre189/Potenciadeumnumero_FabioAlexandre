9508a6cf7fe0585e1f96b1b85e238f68c6b72360
fabio alexandre <73499763+FabioAlexandre189@users.noreply.github.com>
2020-11-23 23:47:04 -0300

    potenciadeumnumero_FabioAlexandre

---
+++ Potenciadeumnumero_FabioAlexandre.java
@@ -0,0 +1,20 @@
+
+package potenciadeumnumero_fabioalexandre;
+
+import java.util.Scanner;
+public class Potenciadeumnumero_FabioAlexandre {
+
+   
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner (System.in);
+         System.out.println("Digite um numero");
+         int numero = leitor.nextInt();
+         int potencia;
+         System.out.println("A potência de 0 a 15 do numero é: ");
+         for (int 1=0; 1<=15;1++)
+         {potencia = (int)Math.pow(numero,i);
+         System.out.println(numero + "x" + 1+ "="+ potencia);
+
+    }
+    } 
+}
